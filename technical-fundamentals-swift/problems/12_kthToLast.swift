403136047aeafe4e01d157851d88527623148077
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-13 17:55:09 -0300

    solution for kthToLast

--- technical-fundamentals-swift/problems/12_kthToLast.swift
+++ technical-fundamentals-swift/problems/12_kthToLast.swift
@@ -3,6 +3,29 @@
 // Implement an algorithm to find the kth to last element of a singly linked list.
 
 func kthToLast<T: Equatable>(_ head: Node<T>?, _ k: Int) -> Node<T>? {
-    // TODO: Implement kth to last functionality
+    guard k > 0 else { return nil }
+    let ret: Node<T>?
+    var curr: Node<T>? = head
+    var count = 0
+
+    while curr != nil {
+        count += 1
+        curr = curr?.next
+    }
+
+    let element = count + 1 - k
+    count = 1
+    curr = head
+    while curr != nil {
+        if count == element {
+            ret = curr
+            return ret
+        }
+
+        curr = curr?.next
+        count += 1
+    }
+
+
     return nil
 }
