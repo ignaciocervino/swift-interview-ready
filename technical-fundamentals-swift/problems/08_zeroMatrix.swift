dc76ea820404e6df127669f56660c52881d1dfcf
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-13 10:29:51 -0300

    Solution for zeroMatrix

--- technical-fundamentals-swift/problems/08_zeroMatrix.swift
+++ technical-fundamentals-swift/problems/08_zeroMatrix.swift
@@ -3,5 +3,22 @@
 // Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0.
 
 func zeroMatrix(_ matrix: inout [[Int]]) {
+    var zeroIndices = [(Int, Int)]()
+    let M = matrix.count
+    let N = matrix[0].count
     
+    for i in 0 ..< M {
+        for j in 0 ..< N {
+            if matrix[i][j] == 0 {
+                zeroIndices.append((i,j))
+            }
+        }
+    }
+
+    for i in 0 ..< M {
+        for j in 0 ..< N {
+            guard zeroIndices.contains(where: { $0.0 == i || $0.1 == j }) else { continue }
+            matrix[i][j] = 0
+        }
+    }
 }
