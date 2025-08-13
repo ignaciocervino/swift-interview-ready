2d324a2dd86a65abfe92981f03ece1f7bf7ea155
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-13 00:17:45 -0300

    Solution for checkPermutations

--- technical-fundamentals-swift/problems/02_checkPermutations.swift
+++ technical-fundamentals-swift/problems/02_checkPermutations.swift
@@ -3,5 +3,16 @@
 // Given two strings, write a method to decide if one is a permutation of the other.
 
 func checkPermutations(_ s1: String, _ s2: String) -> Bool {
-    return false
+    var ocurrencies = [Character: Int]()
+
+    for letter in s1 {
+        ocurrencies[letter, default: 0] += 1
+    }
+
+    for letter in s2 {
+        guard ocurrencies[letter] != nil else { return false }
+        ocurrencies[letter]! -= 1
+    }
+
+    return ocurrencies.values.reduce(0, +) == 0
 }
