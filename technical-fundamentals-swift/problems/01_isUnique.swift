deeeece298a799ba26759539e9c22bb837474bbc
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-13 00:08:55 -0300

    solutions isUnique

--- technical-fundamentals-swift/problems/01_isUnique.swift
+++ technical-fundamentals-swift/problems/01_isUnique.swift
@@ -4,5 +4,18 @@
 // What if you cannot use additional data structures?
 
 func isUnique(_ str: String) -> Bool {
-    return false
+    // Most simple solution
+    // return Set(str).count == str.count
+
+    // Naive solution O(n^2)
+    for (i, letter) in str.enumerated() {
+        let nextIndex = str.index(str.startIndex, offsetBy: i + 1)
+        for letter2 in str[nextIndex..<str.endIndex] {
+            if letter == letter2 {
+                return false
+            }
+        }
+    }
+    return true
 }
+
