a685fcef6188ecf4176e7df0bbbbfa09fff98c16
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-13 01:17:53 -0300

    Solution for one away

--- technical-fundamentals-swift/problems/05_oneAway.swift
+++ technical-fundamentals-swift/problems/05_oneAway.swift
@@ -5,5 +5,35 @@
 // Given two strings, write a function to check if they are one edit (or zero edits) away.
 
 func isOneAway(_ str1: String, _ str2: String) -> Bool {
-    return false
+    var editCount = 0
+    var index1 = 0
+    var index2 = 0
+
+    while index1 < str1.count || index2 < str2.count {
+        guard editCount <= 1 else { return false }
+        guard index1 < str1.count, index2 < str2.count else { 
+            index1 += 1
+            index2 += 1
+            editCount += 1
+            continue
+        }
+        let char1 = str1[str1.index(str1.startIndex, offsetBy: index1)]
+        let char2 = str2[str2.index(str2.startIndex, offsetBy: index2)]
+
+        if char1 != char2 {
+            editCount += 1
+            if str1.count > str2.count {
+                index1 += 1
+                continue
+            } else if str1.count < str2.count {
+                index2 += 1
+                continue
+            }
+        }
+
+        index1 += 1
+        index2 += 1
+    }
+
+    return editCount <= 1
 }
