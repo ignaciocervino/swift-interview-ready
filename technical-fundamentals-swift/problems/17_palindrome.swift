b9a301a06ef99604b0e59a1942a68be4a67cffce
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-18 11:03:21 -0300

    solution for palindrome linked list

--- technical-fundamentals-swift/problems/17_palindrome.swift
+++ technical-fundamentals-swift/problems/17_palindrome.swift
@@ -3,6 +3,11 @@
 // Implement a function to check if a linked list is a palindrome.
 
 func isPalindrome<T: Equatable & Hashable>(_ head: Node<T>?) -> Bool {
-    // TODO: Implement palindrome check functionality
-    return false
+    var array = Array<T>()
+
+    head?.forEach { node in 
+        array.append(node.value)
+    }
+
+    return array == array.reversed()
 }
