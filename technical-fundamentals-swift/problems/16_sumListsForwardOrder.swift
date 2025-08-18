db91b7d39aae2dd28ed161040c40d79c325f6198
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-18 10:59:04 -0300

    solution for sumListForwardOrder

--- technical-fundamentals-swift/problems/16_sumListsForwardOrder.swift
+++ technical-fundamentals-swift/problems/16_sumListsForwardOrder.swift
@@ -7,6 +7,32 @@
 // ```
 
 func sumListsForwardOrder(_ list1: Node<Int>?, _ list2: Node<Int>?) -> Node<Int>? {
-    // TODO: Implement sum lists forward order functionality
-    return nil
+    var sum1 = 0
+    var sum2 = 0
+    var multiplier = 1
+    var total = 0
+
+    list1?.forEach { node in 
+        sum1 *= multiplier
+        sum1 += node.value
+        multiplier = 10
+    }
+
+    multiplier = 1
+
+    list2?.forEach { node in
+        sum2 *= multiplier
+        sum2 += node.value
+        multiplier = 10
+    }
+
+    total = sum1 + sum2
+    var digit = total % 10
+    var sumList = LinkedList<Int>()
+    while total > 0 {
+        sumList.push(digit)
+        total = total / 10
+        digit = total % 10
+    }
+    return sumList.head
 }
