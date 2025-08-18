08d6a9bbf73b74fb0edf95d112fc4f84d3c3d661
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-18 10:50:01 -0300

    solution for sumLists

--- technical-fundamentals-swift/problems/15_sumLists.swift
+++ technical-fundamentals-swift/problems/15_sumLists.swift
@@ -10,6 +10,30 @@
 // ```
 
 func sumLists(_ list1: Node<Int>?, _ list2: Node<Int>?) -> Node<Int>? {
-    // TODO: Implement sum lists functionality
-    return nil
+    var sum1 = 0
+    var sum2 = 0
+    var multiplier = 1
+    var total = 0
+
+    list1?.forEach { node in 
+        sum1 += multiplier * node.value
+        multiplier *= 10
+    }
+
+    multiplier = 1
+
+    list2?.forEach { node in
+        sum2 += multiplier * node.value
+        multiplier *= 10
+    }
+
+    total = sum1 + sum2
+    var digit = total % 10
+    var sumList = LinkedList<Int>()
+    while total > 0 {
+        sumList.append(value: digit)
+        total = total / 10
+        digit = total % 10
+    }
+    return sumList.head
 }
