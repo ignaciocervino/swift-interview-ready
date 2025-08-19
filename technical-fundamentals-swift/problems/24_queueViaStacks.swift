8424591160fb7cbbfafba7fac7c7dc6888f3f8f5
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-19 19:18:55 -0300

    Solution for problem 24

--- technical-fundamentals-swift/problems/24_queueViaStacks.swift
+++ technical-fundamentals-swift/problems/24_queueViaStacks.swift
@@ -2,28 +2,88 @@
 
 // Implement a MyQueue class which implements a queue using two stacks.
 
+// MARK: - Second attempt understanding we should have a stack for pushes and other for pops
 class MyQueue<T> {
+    private var inStack = [T]()
+    private var outStack = [T]()
     
     init() {
-        // TODO: Implement queue using two stacks
+
     }
     
     func enqueue(_ value: T) {
-        // TODO: Implement enqueue functionality
+        inStack.append(value)
     }
     
     func dequeue() -> T? {
-        // TODO: Implement dequeue functionality
-        return nil
+        var aux = [T]()
+        if outStack.isEmpty {
+            while !inStack.isEmpty {
+                aux.append(inStack.popLast()!)
+            }
+
+            for element in aux {
+                outStack.append(element)
+            }
+
+            return outStack.popLast()
+        }
+
+        return outStack.popLast()
     }
     
     func peek() -> T? {
-        // TODO: Implement peek functionality
-        return nil
+        if !outStack.isEmpty {
+            return outStack.last
+        }
+        return inStack.first
     }
     
     func isEmpty() -> Bool {
-        // TODO: Implement isEmpty functionality
-        return true
+        inStack.isEmpty && outStack.isEmpty
     }
 }
+
+// MARK: - My first attempt
+// class MyQueue<T> {
+//     private var peekStack = [T]()
+//     private var mainStack = [T]()
+    
+//     init() {
+
+//     }
+    
+//     func enqueue(_ value: T) {
+//         var aux = [T]()
+//         if peekStack.isEmpty {
+//             peekStack.append(value)
+//         } else {
+//             while !mainStack.isEmpty {
+//                 aux.append(mainStack.popLast()!)
+//             }
+//             mainStack.append(value)
+//             for element in aux {
+//                 mainStack.append(element)
+//             }
+//         }
+//     }
+    
+//     func dequeue() -> T? {
+//         var ret: T?
+//         if !peekStack.isEmpty {
+//             ret = peekStack.popLast()
+//             if !mainStack.isEmpty {
+//                 peekStack.append(mainStack.popLast()!)
+//             }
+//         }
+//         return ret
+//     }
+    
+//     func peek() -> T? {
+//         peekStack.last
+//     }
+    
+//     func isEmpty() -> Bool {
+//         peekStack.isEmpty
+//     }
+// }
