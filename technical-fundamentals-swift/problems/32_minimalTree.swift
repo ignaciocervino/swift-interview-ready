55339d150c090e83a7335b8b75a9bf29e674c657
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-20 19:37:59 -0300

    Solution for problem 32

--- technical-fundamentals-swift/problems/32_minimalTree.swift
+++ technical-fundamentals-swift/problems/32_minimalTree.swift
@@ -1,6 +1,31 @@
-// Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
+// 2. *Minimal Tree*:
+
+// Given a sorted (increasing order) array with unique integer elements,
+// write an algorithm to create a binary search tree with minimal height.
+//
+// A binary search tree is a search where for each node,
+// lesser elements are on the left node, and greater elements on the right node.
+//
+// Input: [1,2,3,4,5,6,7,8]
+// Output:
+//      5
+//   2  |  7
+// 1   3|6   8
+//
+//
 
 public func minimalTree<T: Equatable & Hashable>(_ sortedArray: [T]) -> TreeNode<T>? {
-    // TODO: Implement minimal tree creation algorithm
-    return nil
+    guard !sortedArray.isEmpty else { return nil }
+    return minimalBST(sortedArray, start: 0, end: sortedArray.count - 1)
+}
+
+public func minimalBST<T: Equatable & Hashable>(_ array: [T], start: Int, end: Int) -> TreeNode<T>? {
+    guard start <= end else { return nil }
+    let middle = (start + end + 1) / 2
+    let node = TreeNode(value: array[middle])
+
+    node.left = minimalBST(array, start: start, end: middle - 1)
+    node.right = minimalBST(array, start: middle + 1, end: end)
+
+    return node
 }
