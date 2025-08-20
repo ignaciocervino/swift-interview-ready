5eba1be970c89d49a1060f11c1a84c351c755137
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-20 18:38:12 -0300

    Solution for problem 31

--- technical-fundamentals-swift/problems/31_hasRouteBetweenNodes.swift
+++ technical-fundamentals-swift/problems/31_hasRouteBetweenNodes.swift
@@ -1,6 +1,9 @@
-// Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+// 1. *Route Between Nodes*:
 
-public class GraphNode<T: Equatable & Hashable> {
+// Given a directed graph, design an algorithm to find out whether there is a route
+// between two nodes.
+
+public class GraphNode<T: Equatable & Hashable>: Hashable, Equatable {
     public var value: T
     public var neighbors: [GraphNode<T>]
     
@@ -8,9 +11,29 @@ public class GraphNode<T: Equatable & Hashable> {
         self.value = value
         self.neighbors = []
     }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+
+    public static func == (lhs: GraphNode<T>, rhs: GraphNode<T>) -> Bool {
+        lhs === rhs
+    }
 }
 
 public func hasRouteBetweenNodes<T: Equatable & Hashable>(start: GraphNode<T>, end: GraphNode<T>) -> Bool {
-    // TODO: Implement route finding algorithm
+    var visited: Set<GraphNode<T>> = []
+    return dfs(start, end, &visited)
+}
+
+public func dfs<T: Equatable & Hashable>(_ node: GraphNode<T>, _ target: GraphNode<T>, _ visited: inout Set<GraphNode<T>>) -> Bool {
+    if node === target { return true }
+    visited.insert(node)
+    for neighbor in node.neighbors {
+        if !visited.contains(neighbor) {
+            if dfs(neighbor, target, &visited) { return true }
+        }
+    }
+
     return false
 }
