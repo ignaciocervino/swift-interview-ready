697686a88b8c43d6d4a18acd8016c96ec8850ec9
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-21 11:04:59 -0300

    Solution for problem 35

--- technical-fundamentals-swift/problems/35_validateBST.swift
+++ technical-fundamentals-swift/problems/35_validateBST.swift
@@ -1,6 +1,22 @@
-// Validate BST: Implement a function to check if a binary tree is a binary search tree.
+// 5. *Validate BST*:
 
-public func validateBST<T: Equatable & Hashable>(_ node: TreeNode<T>?) -> Bool {
-    // TODO: Implement BST validation algorithm
-    return false
+// Implement a function to check if a binary tree is a binary search tree.
+
+public func validateBST<T: Equatable & Hashable & Comparable>(_ node: TreeNode<T>?) -> Bool {
+    guard let node else { return true }
+    return isValidBST(node, nil, nil)
+}
+
+public func isValidBST<T: Equatable & Hashable & Comparable>(_ node: TreeNode<T>?, _ lowerBound: T?, _ upperBound: T?) -> Bool {
+    guard let node else { return true }
+    
+    if let upperBound, node.value >= upperBound {
+        return false
+    }
+
+    if let lowerBound, node.value <= lowerBound {    
+        return false
+    }
+
+    return isValidBST(node.left, lowerBound, node.value) && isValidBST(node.right, node.value, upperBound)
 }
