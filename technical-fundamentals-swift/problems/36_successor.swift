4d3d983e86dea87c44b8f2d51337be54b7ea5ffc
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-21 11:06:24 -0300

    Update headers

--- technical-fundamentals-swift/problems/36_successor.swift
+++ technical-fundamentals-swift/problems/36_successor.swift
@@ -1,4 +1,8 @@
-// Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a binary search tree. 
+// 6. *Successor*:
+
+// Write an algorithm to find the "next" node
+// (i.e., in-order successor) of a given node in a binary search tree.
+// You may assume that each node has a link to its parent.
 
 public func successor<T: Equatable & Hashable>(_ root: TreeNode<T>?, _ node: TreeNode<T>) -> TreeNode<T>? {
     // TODO: Implement in-order successor algorithm without parent pointers
