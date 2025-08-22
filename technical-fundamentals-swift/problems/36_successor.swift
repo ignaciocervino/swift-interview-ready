38fbb648f496c944eec692c0ce6bd7d1e74fe8e2
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-21 21:44:46 -0300

    first shoot naive solution for successor problem 36

--- technical-fundamentals-swift/problems/36_successor.swift
+++ technical-fundamentals-swift/problems/36_successor.swift
@@ -5,6 +5,23 @@
 // You may assume that each node has a link to its parent.
 
 public func successor<T: Equatable & Hashable>(_ root: TreeNode<T>?, _ node: TreeNode<T>) -> TreeNode<T>? {
-    // TODO: Implement in-order successor algorithm without parent pointers
-    return nil
+    var array = [TreeNode<T>]()
+    var position: Int? = nil
+    dfsInOrder(root, &array, &position, node)
+    if let position, position < array.count - 1 {
+        return array[position+1]
+    } else {
+        return nil   
+    }
+}
+
+public func dfsInOrder<T: Equatable & Hashable>(_ node: TreeNode<T>?, _ array: inout [TreeNode<T>], _ position: inout Int?, _ requestedNode: TreeNode<T>) {
+    guard let node else { return }
+
+    dfsInOrder(node.left, &array, &position, requestedNode)
+    array.append(node)
+    if node === requestedNode {
+       position = array.count - 1
+    }
+    dfsInOrder(node.right, &array, &position, requestedNode)
 }
