1e7e0a9ac499cfb279f8a9556d307d2c0bb95cfe
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-22 11:02:47 -0300

    Solution for problem 37

--- technical-fundamentals-swift/problems/37_buildOrder.swift
+++ technical-fundamentals-swift/problems/37_buildOrder.swift
@@ -19,6 +19,50 @@ public enum BuildOrderResult {
 }
 
 public func buildOrder(projects: [String], dependencies: [[String]]) -> BuildOrderResult {
-    // TODO: Implement build order algorithm
-    return .error("Not implemented")
+    var nodes = Dictionary<String, GraphNode<String>>()
+    var nodeCount = Dictionary<String, Int>()
+    var buildOrder = [String]()
+
+    for project in projects {
+        if nodes[project] == nil {
+            nodes[project] = GraphNode(value: project)
+            nodeCount[project] = 0 
+        } else {
+            return .error("No valid build order")
+        }
+    }
+
+    for dependency in dependencies {
+        if let node = nodes[dependency[0]], let child = nodes[dependency[1]] {
+            nodeCount[dependency[1], default: 0] += 1 
+            node.neighbors.append(child)
+        } else {
+            return .error("No valid build order")
+        }
+    }
+
+    let rootNodes = projects.filter { nodeCount[$0] == 0 }
+
+    bfs(nodes: rootNodes, nodeCount: &nodeCount, graph: nodes, buildOrder: &buildOrder)
+    if buildOrder.count != projects.count {
+        return .error("No valid build order")
+    } else {
+        return .success(buildOrder)
+    }
+}
+
+func bfs(nodes: [String], nodeCount: inout [String: Int], graph: [String: GraphNode<String>], buildOrder: inout [String]){
+    var queue = nodes
+
+    while !queue.isEmpty {
+        let dequeueValue = queue.removeFirst()
+        buildOrder.append(dequeueValue)
+        for neighbor in graph[dequeueValue]!.neighbors {
+            nodeCount[neighbor.value]! -= 1
+            
+            if nodeCount[neighbor.value] == 0 {
+                queue.append(neighbor.value)
+            }
+        }
+    }
 }
